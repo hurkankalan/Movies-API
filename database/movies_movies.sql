9badecdb950cdb6995bb977d29c8640f8b1e1069
Hurkan Kalan <hurkan.kalan@gmail.com>
2022-04-03 00:09:57 +0200

    add sql files on database folder

---
+++ database/movies_movies.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
+--
+-- Host: localhost    Database: movies
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `movies`
+--
+
+DROP TABLE IF EXISTS `movies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movies` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `director` varchar(255) NOT NULL,
+  `year` varchar(255) NOT NULL,
+  `color` tinyint(1) NOT NULL,
+  `duration` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movies`
+--
+
+LOCK TABLES `movies` WRITE;
+/*!40000 ALTER TABLE `movies` DISABLE KEYS */;
+INSERT INTO `movies` VALUES (1,'Citizen Kane','Orson Wells','1941',0,120),(2,'The Godfather','Francis Ford Coppola','1972',1,180),(3,'test123','test123','test',1,150),(4,'Apocalypse Now','Francis Ford Coppola','1979',1,150),(5,'2001 a space odyssey','Stanley Kubrick','1968',1,160),(6,'The Dark Knight','Christopher Nolan','2008',1,150),(7,'Pat Patrouille !','Keith Chapman','2013',1,20);
+/*!40000 ALTER TABLE `movies` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-03  0:06:53
--- index.js
+++ index.js
@@ -2,58 +2,12 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const db = require("./config/db_config");
-const Joi = require("joi");
 const { setupRoutes } = require("./routes/index-routes");
 
 // Pour qu'Express puisse lire les contenus de requête JSON par défaut, il faut utiliser un middleware Express intégré: express.json().
 app.use(express.json());
 
 setupRoutes(app);
-
-// Route pour créer un nouveau utilisateur dans la BDD avec verification des champs et generation d'erreur si le meme mail user existe deja
-app.post("/api/users", (req, res) => {
-  const { firstname, lastname, email, city, language } = req.body;
-  let errorValidation = null;
-  db.promise()
-    .query("SELECT * FROM users WHERE email = ?", [email])
-    .then((result) => {
-      if (result[0].length !== 0) {
-        //res.status(409).send("Email already exist");
-        return Promise.reject("DUPLICATE_EMAIL");
-      } else {
-        errorValidation = Joi.object({
-          firstname: Joi.string().max(255).required(),
-          lastname: Joi.string().max(255).required(),
-          email: Joi.string().email().max(255).required(),
-          city: Joi.string().max(255).required(),
-          language: Joi.string().max(255).required(),
-        }).validate({ ...req.body }, { abortEarly: false }).error;
-        if (errorValidation) {
-          return Promise.reject("INVALID_DATA");
-        }
-        return db
-          .promise()
-          .query(
-            `INSERT INTO users(firstname, lastname, email, city, language) VALUES(?, ?, ?, ?, ?)`,
-            [firstname, lastname, email, city, language]
-          );
-      }
-    })
-    .then(({ result }) => {
-      // le precedent then renvoie l'id de la requete, pas besoin d'aller chercher dans result.insertId;
-      const data = { result, firstname, lastname, email, city, language };
-      res.status(201).json(data);
-    })
-    .catch((error) => {
-      if (error === "DUPLICATE_EMAIL") {
-        res.status(409).send("Email already exist");
-      } else if (error === "INVALID_DATA") {
-        res.status(422).send("Data invalid");
-      } else {
-        res.status(500).send(`Error saving the user : ${error.message}`);
-      }
-    });
-});
 
 app.listen(port, (err) => {
   if (err) throw err;
